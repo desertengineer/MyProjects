6661b6e4cd5dc633b885296e21d1f5a01000a798
desertengineer <102937158+desertengineer@users.noreply.github.com>
2022-12-24 00:38:22 +0100

    Add files via upload

---
+++ scripts/SliderImages.js
@@ -0,0 +1,18 @@
+/*document.getElementById("pjcts-slider").onload = function() {GetSliderImges()};*/
+
+function GetSliderImges() {
+
+/*var txtFile="txts\SliderImges.txt"
+var myXML=new XMLHttpRequest()
+
+
+myXML.onreadystatechange = function () {if (this.readyState == 4 && this.status == 200) {SliderImgs(this);}};
+myXML.open("GET",txtFile,false);
+myXML.send(); 
+
+function SliderImgs(xml) {
+	var xmlDoc = xml.responseText; 
+document.getElementsByid ("pjcts-slider").innerHTML=xmlDoc;}; */
+document.getElementsByTagName("div")[7].innerHTML="<img class='mySlides' src='images\At_Work-02.jpg' style='width:100%'/><img class='mySlides' src='images\higher_education-01.jpg' style='width:100%'/><img class='mySlides' src='images\renewable_energy-01.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-05.jpg' style='width:100%'/><img class='mySlides' src='images\primary_education-01.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-04.jpg' style='width:100%'/><img class='mySlides' src='images\health-01.jpg' style='width:100%'/><img class='mySlides' src='images\social-01.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-01.jpg' style='width:100%'/><img class='mySlides' src='images\transportation-01.jpg' style='width:100%'/><img class='mySlides' src='images\recreation-01.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-06.jpg' style='width:100%'/><img class='mySlides' src='images\economy-01.jpg' style='width:100%'/><img class='mySlides' src='images\higher_education-02.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-07.jpg' style='width:100%'/><img class='mySlides' src='images\renewable_energy-02.jpg' style='width:100%'/><img class='mySlides' src='images\municipal_services-02.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-03.jpg' style='width:100%'/><img class='mySlides' src='images\primary_education-02.jpg' style='width:100%'/><img class='mySlides' src='images\health-02.jpg' style='width:100%'/><img class='mySlides' src='images\municipal_services-01.jpg' style='width:100%'/><img class='mySlides' src='images\social-02.jpg' style='width:100%'/><img class='mySlides' src='images\transportation-02.jpg' style='width:100%'/><img class='mySlides' src='images\At_Work-08.jpg' style='width:100%'/><img class='mySlides' src='images\recreation-02.jpg' style='width:100%'/><img class='mySlides' src='images\economy-02.jpg' style='width:100%'/> "
+}; 
+/*=============*/
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,61 @@
+var w3 = {};
+w3.slideshow = function (sel, ms, func) {
+  var i, ss, x = w3.getElements(sel), l = x.length;
+  ss = {};
+  ss.current = 1;
+  ss.x = x;
+  ss.ondisplaychange = func;
+  if (!isNaN(ms) || ms == 0) {
+    ss.milliseconds = ms;
+  } else {
+    ss.milliseconds = 1000;
+  }
+  ss.start = function() {
+    ss.display(ss.current)
+    if (ss.ondisplaychange) {ss.ondisplaychange();}
+    if (ss.milliseconds > 0) {
+      window.clearTimeout(ss.timeout);
+      ss.timeout = window.setTimeout(ss.next, ss.milliseconds);
+    }
+  };
+  ss.next = function() {
+    ss.current += 1;
+    if (ss.current > ss.x.length) {ss.current = 1;}
+    ss.start();
+  };
+  ss.previous = function() {
+    ss.current -= 1;
+    if (ss.current < 1) {ss.current = ss.x.length;}
+    ss.start();
+  };
+  ss.display = function (n) {
+    w3.styleElements(ss.x, "display", "none");
+    w3.styleElement(ss.x[n - 1], "display", "block");
+  }
+  ss.start();
+  return ss;
+};
 
-
+w3.includeHTML = function(cb) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          elmnt.removeAttribute("w3-include-html");
+          w3.includeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+};
